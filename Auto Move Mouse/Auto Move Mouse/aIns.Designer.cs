8c8bd2a3b371f6bf08b7583aa828e18254d7a2a5
bJay.Kim <bjay.kim1109@gmail.com>
2018-08-09 23:45:56 +0900

    1. UI Updated 2. CheckBox Function Updated 3. Timer FUnction Updated

---
+++ Auto Move Mouse/Auto Move Mouse/aIns.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Auto_Move_Mouse
+{
+    partial class aIns
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AlwaysCheckBox = new System.Windows.Forms.CheckBox();
+            this.DebugLabel = new System.Windows.Forms.Label();
+            this.OperatingTimer = new System.Windows.Forms.Timer(this.components);
+            this.aInsNotifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // AlwaysCheckBox
+            // 
+            this.AlwaysCheckBox.AutoSize = true;
+            this.AlwaysCheckBox.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.AlwaysCheckBox.Location = new System.Drawing.Point(12, 12);
+            this.AlwaysCheckBox.Name = "AlwaysCheckBox";
+            this.AlwaysCheckBox.Size = new System.Drawing.Size(122, 16);
+            this.AlwaysCheckBox.TabIndex = 0;
+            this.AlwaysCheckBox.Text = "&Always Enable";
+            this.AlwaysCheckBox.UseVisualStyleBackColor = true;
+            this.AlwaysCheckBox.CheckedChanged += new System.EventHandler(this.AlwaysCheckBox_CheckedChanged);
+            // 
+            // DebugLabel
+            // 
+            this.DebugLabel.AutoSize = true;
+            this.DebugLabel.Location = new System.Drawing.Point(12, 31);
+            this.DebugLabel.Name = "DebugLabel";
+            this.DebugLabel.Size = new System.Drawing.Size(56, 12);
+            this.DebugLabel.TabIndex = 1;
+            this.DebugLabel.Text = "DebugStr";
+            // 
+            // aInsNotifyIcon
+            // 
+            this.aInsNotifyIcon.Text = "aIns";
+            this.aInsNotifyIcon.Visible = true;
+            // 
+            // aIns
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(149, 46);
+            this.Controls.Add(this.DebugLabel);
+            this.Controls.Add(this.AlwaysCheckBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "aIns";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "aIns";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.aIns_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.aIns_FormClosed);
+            this.Load += new System.EventHandler(this.aIns_Load);
+            this.Shown += new System.EventHandler(this.aIns_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox AlwaysCheckBox;
+        private System.Windows.Forms.Label DebugLabel;
+        private System.Windows.Forms.Timer OperatingTimer;
+        private System.Windows.Forms.NotifyIcon aInsNotifyIcon;
+    }
+}
+
---
+++ Auto Move Mouse/Auto Move Mouse/aIns.cs
@@ -0,0 +1,159 @@
+﻿#define RELEASE_MODE
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Auto_Move_Mouse
+{
+    public partial class aIns : Form
+    {
+        private const int CONST_TIMER_INTERVAL = 1000;
+        private const int CONST_MOUSE_MOVE_OFFSET = 1;
+
+        private System.Windows.Forms.Timer CheckTimer = null;
+
+        private int TickCounter = 0;
+
+        public aIns()
+        {
+            InitializeComponent();
+        }
+
+        private void aIns_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                this.StartPosition = FormStartPosition.Manual;
+                this.Location = new Point(15, 15);
+#if RELEASE_MODE
+                this.DebugLabel.Visible = false;
+#else
+                this.DebugLabel.Visible = true;
+#endif
+            }
+            catch (System.Exception ex)
+            {
+            }
+        }
+        private void aIns_Shown(object sender, EventArgs e)
+        {
+            try
+            {
+                this.AlwaysCheckBox.Checked = false;
+            }
+            catch (System.Exception ex)
+            {
+            }
+        }
+
+        private void aIns_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+            }
+            catch (System.Exception ex)
+            {
+            }
+        }
+        private void aIns_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            try
+            {
+            }
+            catch (System.Exception ex)
+            {
+            }
+        }
+
+        private void AlwaysCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (this.AlwaysCheckBox.Checked)
+                {
+                    //this.OperatingTimer.Enabled = true;
+                    //this.OperatingTimer.Start();
+
+                    this.CheckTimer = new Timer();
+                    this.CheckTimer.Interval = CONST_TIMER_INTERVAL;
+                    this.CheckTimer.Tick += new System.EventHandler(this.OperatingTimer_Tick);
+
+                    this.CheckTimer.Enabled = true;
+                    this.CheckTimer.Start();
+                }
+                else
+                {
+                    //this.OperatingTimer.Stop();
+                    //this.OperatingTimer.Enabled = false;    
+
+                    this.CheckTimer.Stop();
+                    this.CheckTimer.Enabled = false;
+
+                    this.CheckTimer.Dispose();
+
+                    GC.Collect();
+                }
+            }
+            catch (System.Exception ex)
+            {
+            }
+        }
+
+        private void OperatingTimer_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                switch(this.TickCounter % 4)
+                {
+                    // Left
+                    case 0:
+                        Cursor.Position = new Point(Cursor.Position.X - CONST_MOUSE_MOVE_OFFSET, Cursor.Position.Y);
+                        break;
+
+                    // Up
+                    case 1:
+                        Cursor.Position = new Point(Cursor.Position.X, Cursor.Position.Y - CONST_MOUSE_MOVE_OFFSET);
+                        break;
+
+                    // Right                                                
+                    case 2:
+                        Cursor.Position = new Point(Cursor.Position.X + CONST_MOUSE_MOVE_OFFSET, Cursor.Position.Y);
+                        break;
+
+                    // Down
+                    case 3:
+                        Cursor.Position = new Point(Cursor.Position.X, Cursor.Position.Y + CONST_MOUSE_MOVE_OFFSET);
+                        break;
+
+                    default:
+                        break;
+                }
+
+                if ((int.MaxValue - this.TickCounter) >= 1)
+                {
+                    this.TickCounter += 1;
+                }
+                else
+                {
+                    this.TickCounter = 0;
+                }
+
+#if !RELEASE_MODE
+                Point tPoint = Cursor.Position;
+                this.DebugLabel.Text = string.Format("X:{0:#,0}, Y:{1:#,0}", tPoint.X, tPoint.Y);
+#endif
+            }
+            catch (System.Exception ex)
+            {
+            }
+        }
+    }
+}
